78c85bff27d54a1a542005c311b7b733a6860ba9
JoaN <JoaN@MacBook-Air-de-JoaN.local>
2015-03-04 09:55:46 +0100

    Ejercicio Joan Piera

---
+++ PGtk/PSegunda/MainWindow.cs
@@ -0,0 +1,37 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build (); 
+
+
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButtonAceptarClicked (object sender, EventArgs e)
+	{
+		texto.Buffer.InsertAtCursor ("Hola, Este es el ejercicio!\n");
+
+		if (entryPrimero.Text == "" || entrySegundo.Text == "") {
+			texto.Buffer.InsertAtCursor ("¡¡¡¡Error!!!!\nNo puede estar vacio ningún campo\n");
+		//} else if (entrySegundo.Text == "") {
+		//	texto.Buffer.InsertAtCursor ("La segunda palabra no puede estar vacia");
+		} else {
+			texto.Buffer.InsertAtCursor (entryPrimero.Text+" "+entrySegundo.Text+"\n");
+		}
+
+	}
+	protected void OnButtonBorrarClicked (object sender, EventArgs e)
+	{
+		texto.Buffer.Text = "";
+	}
+}
